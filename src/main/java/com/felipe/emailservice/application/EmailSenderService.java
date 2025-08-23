65178767bc1af742c23b251c2ee9dcfdc4192b9b
Luis Felipe <filipeluisgg+github@gmail.com>
2025-08-23 20:11:01 -0300

    feat: implement application service layer

---
+++ src/main/java/com/felipe/emailservice/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package com.felipe.emailservice.application;
+
+import com.felipe.emailservice.adapters.EmailSenderGateway;
+import com.felipe.emailservice.core.usecases.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase
+{
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailSenderGateway) {
+        this.emailSenderGateway = emailSenderGateway;
+    }
+
+    @Override
+    public void sendEmail(String destiny, String subject, String body) {
+        emailSenderGateway.sendEmail(destiny, subject, body);
+    }
+}
